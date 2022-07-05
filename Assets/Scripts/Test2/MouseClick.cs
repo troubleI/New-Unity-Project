9b70326a09ebafa252256f9cc49e9b92132ed6be
troubleI <jre665599881@qq.com>
2022-07-05 10:41:25 +0800

    second

---
+++ Assets/Scripts/Test2/MouseClick.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    private bool onClick;
+    private Transform cubeTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //≈–∂œ Û±Í◊¥Ã¨
+        if (Input.GetMouseButtonDown(0))
+        {
+            onClick = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            onClick = false;
+            cubeTransform = null;
+        }
+        
+        if (onClick)
+        {
+            if(cubeTransform == null)
+            {
+                IsRayCast();
+            }
+            else
+            {
+                Move();
+                Zoom();
+            }
+        }
+    }
+
+    //≈–∂œ…‰œﬂ «∑Ò…‰µΩcube
+    private void IsRayCast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //ºÏ≤‚…‰œﬂ «∑Ò…‰µΩ
+        if(Physics.Raycast(ray,out hit))
+        {
+            cubeTransform = hit.transform;
+        }
+    }
+
+    //“∆∂Øcube
+    private void Move()
+    {
+        Vector3 cubePos = Camera.main.WorldToScreenPoint(cubeTransform.position);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cubePos.z);
+        cubeTransform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    //Àı∑≈cube
+    private void Zoom()
+    {
+        float scaleNum = Input.GetAxis("Mouse ScrollWheel");
+        if(scaleNum != 0)
+        {
+            scaleNum += 1;
+            Vector3 cubeScale = new Vector3(cubeTransform.localScale.x * scaleNum, cubeTransform.localScale.y * scaleNum, cubeTransform.localScale.z * scaleNum);
+            cubeTransform.localScale = cubeScale;
+        }
+    }
+}
